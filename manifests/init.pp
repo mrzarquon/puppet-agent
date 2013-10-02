c991a53c3f7f0a5b14ad7e973123c5afa304e88f
Chris Barker <cbarker@puppetlabs.com>
2013-10-02 13:14:33 -0700

    working verified version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
 class agent (
-  puppet_server = 'puppet',
-  puppet_ca = 'puppet',
-  puppet_certname = $::fqdn,
-  puppet_environment = $::environment,
-  puppet_start = true,
+  $puppet_server = 'puppet',
+  $puppet_ca = 'puppet',
+  $puppet_certname = $::fqdn,
+  $puppet_environment = 'production',
+  $puppet_start = true,
 ) {
   
   file { '/etc/puppetlabs/puppet/puppet.conf':
     ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => 0644,
+    owner   => pe-puppet,
+    group   => pe-puppet,
+    mode    => 0600,
     content => template('agent/puppet.conf.erb'),
   }
 
